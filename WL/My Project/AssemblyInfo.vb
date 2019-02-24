628ea737ce6cbb815ccab856242aff61bb9cdd04
走过去 戈登 <gordonwalkedby@gmail.com>
2019-02-24 15:13:19 +0800

    v1.0.6

--- WL/My Project/AssemblyInfo.vb
+++ WL/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("2c84b999-5f5e-40f4-8407-0f8d2be20d88")>
+<Assembly: Guid("2c84b9f9-5f5e-40f4-3407-0f8d2be20d88")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.5.0")>
-<Assembly: AssemblyFileVersion("1.0.5.0")>
+<Assembly: AssemblyVersion("1.0.6.0")>
+<Assembly: AssemblyFileVersion("1.0.6.0")>
